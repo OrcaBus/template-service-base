a2f3d22cf17996dd00af58dba2b1e61dc8a85435
Quentin Clayssen <quentin.clayssen@gmail.com>
2026-03-18 12:26:24 +1100

    remove direct deploy mode

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -4,8 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { StatelessStack } from '../infrastructure/toolchain/stateless-stack';
 import { StatefulStack } from '../infrastructure/toolchain/stateful-stack';
 import { TOOLCHAIN_ENVIRONMENT } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
-import { HelloWorldStack } from '../infrastructure/stage/deployment-stack';
-
 const app = new cdk.App();
 
 const deployMode = app.node.tryGetContext('deployMode');
@@ -16,15 +14,6 @@ if (!deployMode) {
 if (deployMode === 'stateless') {
   new StatelessStack(app, 'OrcaBusStatelessHelloWorldStack', {
     env: TOOLCHAIN_ENVIRONMENT,
-  });
-} else if (deployMode === 'direct') {
-  new HelloWorldStack(app, 'HelloWorldStack', {
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION ?? 'ap-southeast-2',
-    },
-    mainBusName: process.env.EVENT_BUS_NAME ?? 'OrcaBusMain',
-    stage: 'dev',
   });
 } else if (deployMode === 'stateful') {
   new StatefulStack(
